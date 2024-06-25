4726b98d72849b6c733268207763d3d7fde740eb
Joshtri <stuffofyos1516@gmail.com>
2024-06-25 22:06:05 +0800

    lts

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -12,12 +12,12 @@ const connectDB = async () => {
         const conn = await mongoose.connect(process.env.MONGODB_URI, {
             // Tambahkan opsi timeout di sini (misalnya, 30 detik)
             // serverSelectionTimeoutMS: 30000,
-        });
+        }); 
 
         console.log(`Database connected. ${conn.connection.host}`);
     } catch (error) {
         console.error(error);
     }
-};
+};  
 
 export default connectDB;
