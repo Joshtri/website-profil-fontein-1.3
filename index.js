e116b4f83b3a7ed06f7d634fde524c8521c8d29d
Joshtri <stuffofyos1516@gmail.com>
2024-06-27 00:14:21 +0800

    fix error page

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
-// import createError from 'http-errors';
+// Import modules
 import express from 'express';
 import path from 'path';
-
 import { config } from 'dotenv';
-
 import { fileURLToPath } from 'url';
-
-
+import createError from 'http-errors'; // Import createError for handling errors
 import connectDB from './config/dbConfig.js';
-
 import indexRouter from './routes/index.route.js';
 import statistikRouter from './routes/statistik.route.js';
-
 
 // Connect to MongoDB
 connectDB();
@@ -25,9 +20,7 @@ const app = express();
 const viewsDirectories = [
     path.join(__dirname, 'views'),
     path.join(__dirname, 'views', 'statistik'),
-
 ];
-
 
 // view engine setup
 app.set('views', viewsDirectories);
@@ -35,14 +28,24 @@ app.set('view engine', 'ejs');
 
 const port = process.env.PORT || 3000;
 
-// app.use(favicon(path.join(__dirname, 'public', 'images', '489px-LOGO_KOTA_KUPANG (1 300x300).png')));
-
-
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 app.use('/', indexRouter, statistikRouter);
 
+// Middleware untuk menangani kesalahan 404
+app.use((req, res, next) => {
+  next(createError(404));
+});
 
+// Middleware untuk menangani kesalahan
+app.use((err, req, res, next) => {
+  // Atur status kesalahan
+  res.status(err.status || 500);
+  // Render halaman kesalahan
+  res.render('error', {
+    message: err.message,
+    error: req.app.get('env') === 'development' ? err : {}, // Hanya tampilkan stacktrace di development
+  });
+});
 
 app.listen(port, () => console.log(`Listening on ${port}`));
